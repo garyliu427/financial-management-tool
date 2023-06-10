462942d3b46dfca6a81d68d8fac8c947433cbbd4
Xiaotian Liu <cblxxlx@gmail.com>
2023-06-11 00:26:03 +1000

    switch to render

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://cryptic-scrubland-51274.herokuapp.com/",
+  baseURL: "https://financial-management-m2kl.onrender.com/",
 });
 
 export default instance;
